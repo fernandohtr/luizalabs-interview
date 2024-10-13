02f5f1b0baae4b9bfd99ca7bfd8c595b57334964
Fernando Toledo <fernandohtr@gmail.com>
2024-10-13 20:29:41 -0300

    build: configure docker compose

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
+DOCKER_LOCAL:=docker compose -f local.yml
+
 format:
 	@ruff check . --fix && ruff format .
 
+local:
+	poetry export -f requirements.txt --with dev --without-hashes --without-urls -o requirements/local.txt
+
+build:
+	${DOCKER_LOCAL} up -d --build --remove-orphans
+
+up:
+	${DOCKER_LOCAL} up -d
+
+stop:
+	${DOCKER_LOCAL} stop
+
+down:
+	${DOCKER_LOCAL} down
+
+logs:
+	@${DOCKER_LOCAL} logs -f
+
 test:
-	@pytest --cov .
+	${DOCKER_LOCAL} run --rm api pytest -p no:warnings --cov=/app/
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -48,8 +48,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_DB"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
